376bd82855a2b1dfb065d0e64c0501b874006442
awcl <113561942+awcl@users.noreply.github.com>
2022-11-15 10:32:10 -0700

    features, wow

--- server.js
+++ server.js
@@ -4,18 +4,22 @@ import cookieParser from 'cookie-parser';
 const app = express();
 app.use(cookieParser());
 
+app.get('/', (req, res)  => {
+  res.status(200).send(`We've been trying to reach you concerning your vehicle's extended warranty. You should've received a notice in the mail about your car's extended warranty eligibility. Since we've not gotten a response, we're giving you a final courtesy call before we close out your file. Press 2 to be removed and placed on our do-not-call list. To speak to someone about possibly extending or reinstating your vehicle's warranty, press 1 to speak with a warranty specialist....Valid paths: login/:name, /hello, and /logout`).end();
+})
+
 app.get('/login/:name', (req, res)  => {
   if (!req.cookies.name)  {
     res.cookie('name', req.params.name).cookie('created', new Date().toISOString());
     res.status(200).send(`${req.params.name}...I have to create a cookie on your machine`).end();
   } else {
-    res.status(200).send(`${req.cookies.name}...you're already signed in so I'm not going to sign you in as "${req.params.name}" 👀 try logging out via /logout please`).end();
+    res.status(403).send(`${req.cookies.name}...you're already signed in so I'm not going to sign you in as "${req.params.name}" 👀 try logging out via /logout please`).end();
   }
 });
 
 app.get('/hello', (req, res)  => {
   req.cookies.name ?  res.status(200).send(`hello ${req.cookies.name}...your cookie was created at ${req.cookies.created}`).end() :
-    res.status(200).send(`Looks like we don't have a cookie yet, try /login/:name`).end()
+    res.status(401).send(`Looks like you're not signed in, try /login/:name`).end()
 });
 
 app.get('/logout', (req, res)  => {
@@ -23,7 +27,7 @@ app.get('/logout', (req, res)  => {
     res.clearCookie('name').clearCookie('created');
     res.status(200).send(`You've been signed out, ${req.cookies.name}`).end();
   } else {
-    res.status(200).send(`You weren't signed in 👀`).end();
+    res.status(401).send(`You weren't signed in 👀`).end();
   }
 });
 
