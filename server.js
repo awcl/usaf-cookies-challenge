d1c7360017785d89df692e17097f5a5f80efd5b7
awcl <113561942+awcl@users.noreply.github.com>
2022-11-15 10:22:04 -0700

    added already logged in condition and logout cape

--- server.js
+++ server.js
@@ -5,13 +5,26 @@ const app = express();
 app.use(cookieParser());
 
 app.get('/login/:name', (req, res)  => {
-  res.cookie("name", req.params.name).cookie("created", new Date().toISOString());
-  res.status(200).send(`${req.params.name}...I have to create a cookie on your machine since you navigated here`).end();
+  if (!req.cookies.name)  {
+    res.cookie('name', req.params.name).cookie('created', new Date().toISOString());
+    res.status(200).send(`${req.params.name}...I have to create a cookie on your machine`).end();
+  } else {
+    res.status(200).send(`${req.cookies.name}...you're already signed in so I'm not going to sign you in as "${req.params.name}" 👀 try logging out via /logout please`).end();
+  }
 });
 
 app.get('/hello', (req, res)  => {
   req.cookies.name ?  res.status(200).send(`hello ${req.cookies.name}...your cookie was created at ${req.cookies.created}`).end() :
     res.status(200).send(`Looks like we don't have a cookie yet, try /login/:name`).end()
+});
+
+app.get('/logout', (req, res)  => {
+  if (req.cookies.name)  {
+    res.clearCookie('name').clearCookie('created');
+    res.status(200).send(`You've been signed out, ${req.cookies.name}`).end();
+  } else {
+    res.status(200).send(`You weren't signed in 👀`).end();
+  }
 });
 
 app.get('*', function (req, res) {
